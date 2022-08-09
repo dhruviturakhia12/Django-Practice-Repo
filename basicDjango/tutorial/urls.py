a18568090abeb2e14110d07bddbaf9eab61610a7
dhruvi <dhruvi.t@auberginesolutions.com>
2022-08-09 18:11:04 +0530

    DAy 1 work

--- basicDjango/tutorial/urls.py
+++ basicDjango/tutorial/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('add/', views.add, name='add'),
+    path('sub/', views.sub, name='sub'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
 ]
--- basicDjango/tutorial/views.py
+++ basicDjango/tutorial/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    # return HttpResponse("Hello! You're at the home finally.")
+    return render(request,'home.html',{'name':'Dhruvi'})
+def about(request):
+    return render(request,'home.html',{'name':'Misha'})
+    # return render(request,'home.html')
+def add(request):
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+    res=val1 + val2
+    return render(request,'result.html',{'result':res})
+    # return render(request,'home.html')
+def sub(request):
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+    res=val1 - val2
+    return render(request,'result.html',{'result':res})
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def contact(request):
+    return HttpResponse("Hello from contact!")
+    # return render(request,'home.html')
+def blog(request):
+    return HttpResponse("Hello from blog!")
+    # return render(request,'home.html')
