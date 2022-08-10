efa18488a305e1d9e2563000c606d2ad498426e7
dhruvi <dhruvi.t@auberginesolutions.com>
2022-08-10 17:43:52 +0530

    Day 2 travel app work

--- basicDjango/tutorial/views.py
+++ basicDjango/tutorial/views.py
@@ -1,26 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
 def home(request):
     # return HttpResponse("Hello! You're at the home finally.")
-    return render(request,'home.html',{'name':'Dhruvi'})
+    return render(request, "home.html", {"name": "Dhruvi"})
+
+
 def about(request):
-    return render(request,'home.html',{'name':'Misha'})
+    return render(request, "home.html", {"name": "Misha"})
     # return render(request,'home.html')
+
+
 def add(request):
-    val1=int(request.POST['num1'])
-    val2=int(request.POST['num2'])
-    res=val1 + val2
-    return render(request,'result.html',{'result':res})
+    val1 = int(request.POST["num1"])
+    val2 = int(request.POST["num2"])
+    res = val1 + val2
+    return render(request, "result.html", {"result": res})
     # return render(request,'home.html')
+
+
 def sub(request):
-    val1=int(request.POST['num1'])
-    val2=int(request.POST['num2'])
-    res=val1 - val2
-    return render(request,'result.html',{'result':res})
+    val1 = int(request.POST["num1"])
+    val2 = int(request.POST["num2"])
+    res = val1 - val2
+    return render(request, "result.html", {"result": res})
+
 
 def contact(request):
     return HttpResponse("Hello from contact!")
     # return render(request,'home.html')
+
+
 def blog(request):
     return HttpResponse("Hello from blog!")
     # return render(request,'home.html')
