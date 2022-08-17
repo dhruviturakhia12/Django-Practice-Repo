08977ed9b9088cf6a1daa6a5dc4fb63bbec04527
dhruvi <dhruvi.t@auberginesolutions.com>
2022-08-17 14:32:39 +0530

    Cleared code for main

--- basicDjango/travellersGuide/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TravellersguideConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'travellersGuide'
--- basicDjango/travellersGuide/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Destination:
-        id: int
-        name: str
-        desc: str
-        img: str
-        price: int
