ad17675c38e1ed46e359c7afe628f516a8deace2
dhruvi <dhruvi.t@auberginesolutions.com>
2022-08-17 14:26:26 +0530

    User login logout

--- basicDjango/basicDjango/urls.py
+++ basicDjango/basicDjango/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("travello/", include("travellersGuide.urls")),
+    path("", include("travellersGuide.urls")),
     path("tutorial/", include("tutorial.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
