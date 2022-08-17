caf1a500f8840932453eb2c6efea7fba1c4c6adc
dhruvi <dhruvi.t@auberginesolutions.com>
2022-08-17 17:28:35 +0530

    Day 5 polls app done

--- basicDjango/basicDjango/settings.py
+++ basicDjango/basicDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'poll',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 MEDIA_URL = "/media/"
